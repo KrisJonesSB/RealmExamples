168f1ab05764d20335b4ae0e3055d71f4abcad83
Kris Jones <kris@securebroadcast.com>
2017-01-22 18:45:11 +0000

    Initial commit

---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'RealmExamples' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for RealmExamples
+  pod 'RealmSwift'
+  pod 'SwiftyJSON', '~> 3.1.1'
+
+end
---
+++ RealmExamples/AppDelegate.swift
@@ -0,0 +1,73 @@
+//
+//  AppDelegate.swift
+//  RealmExamples
+//
+//  Created by Kris Jones on 22/01/2017.
+//  Copyright © 2017 NSBelfast. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    
+     setupRealm()
+    
+    return true
+  }
+  
+  func setupRealm() {
+    // Migrate the Realm schema
+    runRealmDataMigration()
+    // Now that we've told Realm how to handle the schema change, opening the file
+    // will automatically perform the migration
+    _ = try! Realm()
+  }
+  
+  func runRealmDataMigration() {
+    let config = Realm.Configuration(
+      // Set the new schema version. This must be greater than the previously used
+      // version (if you've never set a schema version before, the version is 0).
+      schemaVersion: 1,
+      
+      // Set the block which will be called automatically when opening a Realm with
+      // a schema version lower than the one set above
+      migrationBlock: { migration, oldSchemaVersion in
+        // We haven’t migrated anything yet, so oldSchemaVersion == 0
+        if (oldSchemaVersion < 2) {
+          // Nothing to do!
+          // Realm will automatically detect new properties and removed properties
+          // And will update the schema on disk automatically
+          
+          // See https://realm.io/docs/swift/latest/#migrations for more detail on performing data migrations
+        }
+    })
+    
+    // Tell Realm to use this new configuration object for the default Realm
+    Realm.Configuration.defaultConfiguration = config
+  }
+
+  func applicationWillResignActive(_ application: UIApplication) {
+  }
+
+  func applicationDidEnterBackground(_ application: UIApplication) {
+  }
+
+  func applicationWillEnterForeground(_ application: UIApplication) {
+  }
+
+  func applicationDidBecomeActive(_ application: UIApplication) {
+  }
+
+  func applicationWillTerminate(_ application: UIApplication) {
+  }
+
+
+}
+
