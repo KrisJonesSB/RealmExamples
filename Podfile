9af49bda024bfb572ff94917f734b5dbdfce99ac
Kris Jones <kris@securebroadcast.com>
2017-01-23 08:38:57 +0000

    Adding shared pods to test target

--- Podfile
+++ Podfile
@@ -1,12 +1,24 @@
 # Uncomment the next line to define a global platform for your project
 # platform :ios, '9.0'
 
+def shared_pods
+  pod 'RealmSwift'
+  pod 'SwiftyJSON', '~> 3.1.1'
+end
+
 target 'RealmExamples' do
   # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
   use_frameworks!
 
   # Pods for RealmExamples
-  pod 'RealmSwift'
-  pod 'SwiftyJSON', '~> 3.1.1'
+  shared_pods
+end
+
+target 'RealmExamplesTests’ do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for RealmExamples
+  shared_pods
 
 end
---
+++ RealmExamples/Petition.swift
@@ -0,0 +1,45 @@
+//
+//  HistoricEvent.swift
+//  RealmExamples
+//
+//  Created by Kris Jones on 22/01/2017.
+//  Copyright © 2017 NSBelfast. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+import SwiftyJSON
+
+
+class Petition : Object {
+  
+  
+  dynamic var petitionId = 0
+  dynamic var action: String?
+  dynamic var background: String?
+  dynamic var state: String?
+  dynamic var signatureCount = 0
+  dynamic var createdAt: Date?
+  dynamic var closedAt: Date?
+  dynamic var creatorName: String?
+  dynamic var governmentResponse: GovernmentResponse?
+  dynamic var signed = 0
+  
+  func initWith(json: JSON) {
+    
+    self.petitionId = json["id"].int ?? 0
+    self.action = json["attributes"]["action"].string
+    self.creatorName = json["attributes"]["creator_name"].string
+    self.state = json["attributes"]["state"].string
+    self.background = json["attributes"]["background"].string
+    self.createdAt = json["attributes"]["created_at"].dateTime
+    self.closedAt = json["attributes"]["closed_at"].dateTime
+    self.signatureCount = json["attributes"]["signature_count"].int ?? 0
+    
+    if let _ = json["attributes"]["government_response"].dictionary {
+      let governmentResponse = GovernmentResponse()
+      governmentResponse.initWith(json: json["attributes"]["government_response"])
+      self.governmentResponse = governmentResponse
+    }
+  }
+}
